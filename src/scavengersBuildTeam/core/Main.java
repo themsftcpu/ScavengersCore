9e539cc1e4e5eeedfe71561ec36b5eb88d7d4cca
themsftcpu <computerizedtaco@gmail.com>
2013-08-06 19:14:58 -0400

    Edited onEnable() Message.
    Edited onDisable() Message.
    
    Began Work on command /ILikeBoh
    
    +--------------------+
    | Version: 0.2 Dev   |
    | Build: 007         |
    | Changes Made:      |
    | 8\6 Main.java      |
    | 8\6 plugin.yml     |
    x--------------------x

--- src/scavengersBuildTeam/core/Main.java
+++ src/scavengersBuildTeam/core/Main.java
@@ -2,7 +2,7 @@ package scavengersBuildTeam.core;
 
 import java.util.logging.Logger;
 
-import org.bukkit.ChatColor;
+//import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -15,19 +15,33 @@ public class Main extends JavaPlugin{
 	
 	public void onDisable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info("[Scavengers Plugin]: " + pdfFile.getName() + "Has been disabled.");
+		logger.info("+----- [SCAVENGERS PLUGIN] -----+");
+		logger.info("| Uh-oh, did you restart me?    |");
+		logger.info("| Version "+ pdfFile.getVersion() +" Has been disabled.|");
+		logger.info("| 'Mumble, what did you do to   |");
+		logger.info("|  the server?' - Cpu x9001     |");
+		logger.info("+-------------------------------|");
 	}
 	
 	public void onEnable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info("[Scavengers Plugin]: " + pdfFile.getName() + "Version " + pdfFile.getVersion() + "Has been disabled.");
+		logger.info("+----- [SCAVENGERS PLUGIN] -----+");
+		logger.info("| Developed in-house by thecpu  |");
+		logger.info("| Version "+ pdfFile.getVersion() +" Has been enabled. |");
+		logger.info("| 'Houston, we are online and   |");
+		logger.info("|  ready to go.' - No one ever  |");
+		logger.info("+-------------------------------|");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		Player player = (Player) sender;
 		if(commandLabel.equals("WhoIsBoh")){
-			player.sendMessage(ChatColor.RED + "A bad person. If you ever see him, tell him he sucks."); //Mumble, tell me what to put in the quotes.
+			player.sendMessage("A bad person. If you ever see him, tell him he sucks.");
 		}
+		else if(commandLabel.equals("ILikeBoh")){
+			player.sendMessage("How can you?");
+			player.setHealth(0); 
+		} //If you didn't understand that, it means if the player types /ILikeBoh they get killed.
 		return false;
 	}
 }
